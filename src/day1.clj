6ccb987e09b1143a75e77be1ba2963b036693e83
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2020-12-04 20:20:42 +0100

    day1 part2

--- src/day1.clj
+++ src/day1.clj
@@ -1,9 +1,21 @@
 (ns day1
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [split-lines]]
+            [clojure.math.combinatorics :as combo]))
 
+(def example '#{1721 979 366 299 675 1456})
 (def expenses
   (into #{}
         (map #(Integer/parseInt %)
              (split-lines (slurp "./src/day1.input.txt")))))
 
+;; part one
+(apply * (filter #(contains? example (- 2020 %)) example))
 (apply * (filter #(contains? expenses (- 2020 %)) expenses))
+
+;; part two
+(apply * (first (filter #(= 2020 (apply + %))
+                        (combo/combinations example 3))))
+(apply * (first (filter #(= 2020 (apply + %))
+                        (combo/combinations expenses 3))))
+
+
