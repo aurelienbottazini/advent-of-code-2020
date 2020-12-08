05b69a6b80961c870134035f949b4646719ca057
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2020-12-08 10:29:57 +0100

    day8 part2

--- src/day8.clj
+++ src/day8.clj
@@ -1,24 +1,48 @@
 (ns day8)
 
 (def example (clojure.string/split-lines (slurp "./src/day8.example.txt")))
+(def example-fixed (clojure.string/split-lines (slurp "./src/day8.example.fixed.txt")))
 (def input (clojure.string/split-lines (slurp "./src/day8.input.txt")))
 
-(get example 2)
-
 (defn boot
-  ([input] (boot input 0 0 #{}))
-  ([input acc index visited-indexes]
-   (let [[_ op val-string] (re-find #"([a-z]{3}) ([+|-][0-9]+)" (get input index))
+  ([coll] (boot coll 0 0 #{}))
+  ([coll acc index visited-indexes]
+   (let [x (get coll index)
+         [_ op val-string] (and x (re-find #"([a-z]{3}) ([+|-][0-9]+)" x))
          val (clojure.edn/read-string val-string)
-         updated-indexes (conj visited-indexes index)]
+         updated-indexes (and index (conj visited-indexes index))]
      (cond
-       (contains? visited-indexes index) acc
-       (= op "nop") (recur input acc (+ 1 index) updated-indexes)
-       (= op "acc") (recur input (+ acc val) (+ 1 index) updated-indexes)
-       (= op "jmp") (recur input acc (+ index val) updated-indexes)
-       :else (println "unknown op: " updated-indexes)))))
+       (contains? visited-indexes index) [:infinite-loop acc]
+       (= index (count coll)) [:done acc]
+       (= op "nop") (recur coll acc (+ 1 index) updated-indexes)
+       (= op "acc") (recur coll (+ acc val) (+ 1 index) updated-indexes)
+       (= op "jmp") (recur coll acc (+ index val) updated-indexes)
+       :else [:unknown-op updated-indexes]))))
 
+;; part 1
 (boot example)
 (boot input)
 
+;; part 2
+(boot example-fixed)
 
+(defn jump-indexes [coll]
+  (keep-indexed #(when (re-find #"jmp" %2) %1) coll))
+
+(defn boot-fix
+  ([coll] (boot-fix coll (jump-indexes coll) nil))
+  ([coll jumps last-replacement]
+   (let [[status val] (boot coll)]
+     (cond
+       (= :done status) val
+       (= :infinite-loop status) (let [new-replacement [(first jumps) (get coll (first jumps))]
+                                       [last-index last-string] last-replacement
+                                       restored-coll (if last-replacement
+                                                       (assoc coll last-index last-string)
+                                                       coll)]
+                                   (recur (assoc restored-coll (first jumps) "nop +0")
+                                          (rest jumps)
+                                          [(first jumps) (get coll (first jumps))]))))))
+
+(boot-fix example)
+(boot-fix input)
