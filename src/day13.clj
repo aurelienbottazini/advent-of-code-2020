b05fb53c6268a02733634ceaaeb2f9e6067c8d8c
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2020-12-30 18:33:23 +0100

    day13 part2

--- src/day13.clj
+++ src/day13.clj
@@ -25,3 +25,30 @@
                 (map #(closest-bus-time time %) bus-ids)))]
   (* (get bus-with-time 0)
      (- (get bus-with-time 1) time)))
+
+;; part 2
+
+(defn parse-input2 [path]
+  (let [input (->> path slurp clojure.string/split-lines)]
+    [(clojure.edn/read-string (first input))
+     (->> input rest first
+          (#(clojure.string/split % #","))
+          (map #(clojure.edn/read-string %)))]))
+
+(def input2 (parse-input2 "./src/day13.input"))
+
+(defn next-bus [[sum product] [index bus-id]]
+  (loop [sum sum]
+    (if (zero? (mod (+ sum index) bus-id))
+      [sum (* product bus-id)]
+      (recur (+ sum product)))))
+
+(->> input2 rest first
+     (interleave (range))
+     (partition 2)
+     (map (fn [[index bus-id]]
+            (if (not= 'x bus-id)
+              [index bus-id])))
+     (remove nil?)
+     (reduce next-bus)
+     first)
